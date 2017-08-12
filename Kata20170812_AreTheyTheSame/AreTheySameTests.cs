4f3e7d6ada6837f96c9fb4fc516b30d6ca92faf4
cash <cash@cashwu.com>
2017-08-12 17:00:50 +0800

    input 2 and 4 test case

--- Kata20170812_AreTheyTheSame/AreTheySameTests.cs
+++ Kata20170812_AreTheyTheSame/AreTheySameTests.cs
@@ -7,8 +7,25 @@ namespace Kata20170812_AreTheyTheSame
     public class AreTheySameTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_2_and_4_should_return_true()
         {
+            var a = new[] { 2 };
+            var b = new[] { 4 };
+
+            TheySameShouldBeTrue(a, b);
+        }
+
+        private static void TheySameShouldBeTrue(int[] a, int[] b)
+        {
+            Assert.IsTrue(new AreTheySame().comp(a, b));
+        }
+    }
+
+    public class AreTheySame
+    {
+        public bool comp(int[] a, int[] b)
+        {
+            return true;
         }
     }
 }
